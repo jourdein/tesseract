be4e9990203952aed605296b8401254c1dd009ef
Matt.J <mailbox@jourdein.com>
2024-01-04 21:48:12 +0800

    add: dummy data

--- config/initializers/market_info.rb
+++ config/initializers/market_info.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 $real_data = false
 require Rails.root.join('lib', 'market_info', 'client.rb')
---
+++ config/initializers/symbols.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+$symbols = JSON.parse(File.read(File.join(Rails.root, 'config/symbols.json')))
