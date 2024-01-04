61c156588af33b99a7974fba6c6544206f98463c
Matt.J <mailbox@jourdein.com>
2024-01-04 21:49:25 +0800

    add search field

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :rooms do
     resources :messages
   end
 
-  resources :tickers, except: [:update, :edit]
-
-  if Rails.env.development?
-    mount Lookbook::Engine, at: "/lookbook"
+  resources :tickers, except: %i[update edit]
+  scope :search do
+    get :documents, to: 'search#documents'
   end
+
+  mount Lookbook::Engine, at: '/lookbook' if Rails.env.development?
 
   # Defines the root path route ("/")
   # root "articles#index"
